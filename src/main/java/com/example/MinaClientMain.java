192f437900b0fb28821d55b90222cb508a2b80ce
ouyangshixiong <ouyangshixiong@gmail.com>
2019-07-15 18:35:07 +0800

    强制mina同步发送消息测试

--- src/main/java/com/example/MinaClientMain.java
+++ src/main/java/com/example/MinaClientMain.java
@@ -3,6 +3,9 @@ package com.example;
 import com.example.protocol.DemoMsgDto;
 import com.example.protocol.ProtocolWrapper;
 import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.future.IoFuture;
+import org.apache.mina.core.future.IoFutureListener;
+import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.slf4j.Logger;
@@ -23,13 +26,35 @@ public class MinaClientMain {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
         log.info("*************** Demo client start up successfully!***************");
 
-        sendTextMessage(context);
+        //建立多个连接发送请求
+        for(int i=0; i<1; i++){
+            sendAsyncTextMessage(context);
+        }
     }
 
     private static IoSession connectTo(ClassPathXmlApplicationContext context){
         //连接上server
         NioSocketConnector tcpConnector = context.getBean("tcpConnector", NioSocketConnector.class);
         ConnectFuture connectFuture = tcpConnector.connect(address);
+        //同步
+        connectFuture.awaitUninterruptibly();
+        if(connectFuture.isConnected()){
+            log.info("client connected!");
+        }else{
+            log.error("client not connected");
+        }
+        return connectFuture.getSession();
+    }
+
+    private static IoSession connectToAsync(ClassPathXmlApplicationContext context){
+        //连接上server
+        NioSocketConnector tcpConnector = context.getBean("tcpConnector", NioSocketConnector.class);
+        //Session同步
+        tcpConnector.getSessionConfig().setBothIdleTime(0);
+        tcpConnector.getSessionConfig().setUseReadOperation(true);
+        tcpConnector.getSessionConfig().setKeepAlive(true);
+        ConnectFuture connectFuture = tcpConnector.connect(address);
+        //connector同步
         connectFuture.awaitUninterruptibly();
         if(connectFuture.isConnected()){
             log.info("client connected!");
@@ -41,20 +66,37 @@ public class MinaClientMain {
 
     private static void sendDemoMsgDto(ClassPathXmlApplicationContext context){
         IoSession session = connectTo(context);
+        session.getConfig().setIdleTime(IdleStatus.BOTH_IDLE,15);
         DemoMsgDto initMsg = new DemoMsgDto();
         initMsg.setDemoMsg("hello 1");
         ProtocolWrapper protocolWrapper = new ProtocolWrapper();
         protocolWrapper.setCMD("demo-msgdto");
         protocolWrapper.setT(initMsg);
+        protocolWrapper.setSequenceNumber(1L);
         session.write(protocolWrapper);
     }
 
+    // 发送顺序消息
     private static void sendTextMessage(ClassPathXmlApplicationContext context){
         IoSession session = connectTo(context);
+        session.getConfig().setIdleTime(IdleStatus.BOTH_IDLE,15);
         ProtocolWrapper protocolWrapper = new ProtocolWrapper();
         protocolWrapper.setCMD("demo-msg-text");
         protocolWrapper.setT("text message hello!");
+        protocolWrapper.setSequenceNumber(1L);
         session.write(protocolWrapper);
+    }
+
+    private static void sendAsyncTextMessage(ClassPathXmlApplicationContext context){
+        IoSession session = connectToAsync(context);
+        for(int i=0; i<10; i++){
+            ProtocolWrapper protocolWrapper = new ProtocolWrapper();
+            protocolWrapper.setCMD("demo-msg-text");
+            protocolWrapper.setT("text message hello!" + (i+1));
+            protocolWrapper.setSequenceNumber( 1L );
+            session.write(protocolWrapper);
+        }
+
     }
 
 }
--- src/main/java/com/example/iohandler/TestHandlerClient.java
+++ src/main/java/com/example/iohandler/TestHandlerClient.java
@@ -3,6 +3,7 @@ package com.example.iohandler;
 import com.example.protocol.DemoMsgDto;
 import com.example.protocol.ProtocolWrapper;
 import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,45 +18,75 @@ public class TestHandlerClient extends IoHandlerAdapter {
     @Override
     public void messageReceived(IoSession session, Object message) throws Exception {
         ProtocolWrapper protocolWrapper = (ProtocolWrapper) message;
-        String CMD = protocolWrapper.getCMD();
-        if( "demo-msgdto".equals(CMD) ){
-            log.info("messageReceived from server type=demo-msgdto content="
-                    + ( (DemoMsgDto)protocolWrapper.getT()).getDemoMsg()  );
-        }else if( "demo-msg-text".equals(CMD) ){
-            log.info("messageReceived from server type=text content="
-                    + (String) protocolWrapper.getT() );
-        }else {
-            log.error("can not parse CMD from server!" + CMD);
-            return;
-        }
-
-        //准备回包
+        sampling(protocolWrapper);
+        //重新new对象，不服用req
         ProtocolWrapper resp = new ProtocolWrapper();
-        resp.setCMD(CMD);
-        //从控制台接受输入
-        Scanner scanner = new Scanner(System.in);
-        if(scanner.hasNext()){
-            String in = scanner.next();
+        resp.setSequenceNumber(protocolWrapper.getSequenceNumber()+1);
+        resp.setT(protocolWrapper.getT());
+        resp.setCMD(protocolWrapper.getCMD());
+        session.write(resp);
+//        //准备回包
+//        ProtocolWrapper resp = new ProtocolWrapper();
+//        resp.setCMD(CMD);
+//        //从控制台接受输入
+//        Scanner scanner = new Scanner(System.in);
+//        if(scanner.hasNext()){
+//            String in = scanner.next();
+//            if( "demo-msgdto".equals(CMD) ){
+//                DemoMsgDto msg = new DemoMsgDto();
+//                if(in.equalsIgnoreCase("quit")){
+//                    log.info("client退出！");
+//                    System.exit(0);
+//                }else{
+//                    msg.setDemoMsg(in);
+//                }
+//                resp.setT(msg);
+//            } else {
+//                resp.setT(in);
+//            }
+//            session.write(resp);
+//        }
+    }
+
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        //服务端关闭了连接就算了
+        if (session.isClosing() || !session.isConnected()) {
+            ;
+        }
+    }
+
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        log.info("session 空闲 id号为:" + session.getId());
+        session.getService().dispose(false);
+
+    }
+
+    private void sampling(ProtocolWrapper protocolWrapper){
+        //取样率
+        long rate = 100;
+        if( protocolWrapper.getSequenceNumber() % rate == 0 ) {
+            String CMD = protocolWrapper.getCMD();
             if( "demo-msgdto".equals(CMD) ){
-                DemoMsgDto msg = new DemoMsgDto();
-                if(in.equalsIgnoreCase("quit")){
-                    log.info("client退出！");
-                    System.exit(0);
-                }else{
-                    msg.setDemoMsg(in);
-                }
-                resp.setT(msg);
-            } else {
-                resp.setT(in);
+                log.info("messageReceived from server type=demo-msgdto sequenceNumber="
+                        + protocolWrapper.getSequenceNumber() + " content="
+                        + ( (DemoMsgDto)protocolWrapper.getT()).getDemoMsg()  );
+            }else if( "demo-msg-text".equals(CMD) ){
+                log.info("messageReceived from server type=text sequenceNumber="
+                        + protocolWrapper.getSequenceNumber() + "content="
+                        + (String) protocolWrapper.getT() );
+            }else {
+                log.error("can not parse CMD from server!" + CMD);
+                return;
             }
-            session.write(resp);
         }
     }
 
     @Override
     public void messageSent(IoSession session, Object message) throws Exception {
         ProtocolWrapper protocolWrapper = (ProtocolWrapper) message;
-        log.info("client messageSent" + protocolWrapper.getCMD() + "---" + protocolWrapper.getT());
+//        log.info("client messageSent" + protocolWrapper.getCMD() + "---" + protocolWrapper.getT());
         super.messageSent(session, message);
     }
 }
